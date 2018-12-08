30b5cf3b1afd9e931ff054ea3b429fef73cc38e3
b0rey <b0rey.dj@gmail.com>
2018-12-08 13:16:03 +0500

    Separate website to another repository

--- Web/website for borrowers/a/leadview/m.js
+++
@@ -1,4 +0,0 @@
-
-function sleadwork(){				// Иниц работы с текущим лидом
-	lfon(0); $.ajax({ url: turl+bs+'cajx=sleadwork&rnd='+backRnd()});
-}
--- Web/website for borrowers/templates/bootstrap3/alert.php
+++
@@ -1,4 +0,0 @@
-<div class="alert alert-<?= $class ?>">
-    <button type="button" class="close" data-dismiss="alert" aria-hidden="true">&times;</button>
-    <?= $msg ?>
-</div>
